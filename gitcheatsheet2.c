556978bb0a0c2ce24cc5c5e5dcc8cdea8ccf5551
aguilarj17 <aguilarhuertaj17@hotmail.com>
2020-03-17 17:10:18 -0600

    Resume2nd Unit

---
+++ gitcheatsheet2.c
@@ -0,0 +1,32 @@
+git config --list // es para listar
+
+echo// 
+
+git init// crear un sub directorio llamado .git para contener los archivos nmecesarios de un repositorio
+
+git status// determina que archivos estan y en que estado de comando
+
+git add // para especificar y agregar archivos
+
+git commit // patra confirmar los cambios realizados 
+
+git config --global user.name// para introducir tu usuario
+
+git config --global user mail// para introducir tu email
+
+git checkout
+
+git amend 
+
+git reset
+
+git rebase 
+
+git revert 
+
+git commit// para escribir un mensaje 
+
+git remote//
+
+git push origin master //
+
